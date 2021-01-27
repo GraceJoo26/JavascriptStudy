a9e6ad7f117e86b275a6949a4522a1219b5a032b
Gracejoo26 <gracejoo26@gmail.com>
2021-01-27 13:51:46 +0900

    object_book

---
+++ object_01.js
@@ -0,0 +1,16 @@
+//객체선언
+var product = {
+    제품명 : '7D건조망고',
+    유형 : '당절임',
+    성분 : '망고, 설탕, 메타중아황산나트륨, 치자황색소',
+    원산지 : '필리핀'
+}
+
+console.log(product.제품명);
+
+var object = {
+    'with space':273,
+    'with ~!12!@#$@!!@#':52
+};
+console.log(object['with space']);
+console.log(object['with ~!12!@#$@!!@#']);
