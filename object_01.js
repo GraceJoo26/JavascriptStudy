1c5dc3ad92c196e7d574d1a0151c20fd3d18436c
Gracejoo26 <gracejoo26@google.com>
2021-01-31 17:19:50 +0900

    prototype

--- object_01.js
+++ object_01.js
@@ -73,29 +73,29 @@ output += 'total:' + (student.Korean + student.Math + student.English + student.
     }
   */
 
-
+/* 
     var students = [];
-    var student0 ={이름:'주유진',국어:'87',수학:'98',영어:'88',과학:'100'};
-    var student1 ={이름:'주요진',국어:'87',수학:'98',영어:'88',과학:'100'};
-    var student2 ={이름:'주시진',국어:'87',수학:'98',영어:'88',과학:'100'};
-    var student3 ={이름:'주지진',국어:'87',수학:'98',영어:'88',과학:'100'};
-    var student4 ={이름:'주비진',국어:'87',수학:'98',영어:'88',과학:'100'};
-    var student5 ={이름:'주수진',국어:'87',수학:'98',영어:'88',과학:'100'};
-    var student6 ={이름:'주재진',국어:'90',수학:'100',영어:'80',과학:'50'};
-    var student7 ={이름:'주기진',국어:'87',수학:'98',영어:'88',과학:'100'};
-    var student8 ={이름:'주세진',국어:'87',수학:'98',영어:'88',과학:'100'};
-    var student9 ={이름:'주추진',국어:'87',수학:'98',영어:'88',과학:'100'};
+    var student0 ={이름:'주유진',국어:87,수학:98,영어:88,과학:100};
+    var student1 ={이름:'주요진',국어:87,수학:98,영어:88,과학:100};
+    var student2 ={이름:'주시진',국어:87,수학:98,영어:88,과학:100};
+    var student3 ={이름:'주지진',국어:87,수학:98,영어:88,과학:100};
+    var student4 ={이름:'주비진',국어:87,수학:98,영어:88,과학:100};
+    var student5 ={이름:'주수진',국어:87,수학:98,영어:88,과학:100};
+    var student6 ={이름:'주재진',국어:90,수학:100,영어:80,과학:50};
+    var student7 ={이름:'주기진',국어:87,수학:98,영어:88,과학:100};
+    var student8 ={이름:'주세진',국어:87,수학:98,영어:88,과학:100};
+    var student9 ={이름:'주추진',국어:87,수학:98,영어:88,과학:100};
     
-    students.push({이름:'주유진',국어:'87',수학:'98',영어:'88',과학:'100'});
-    students.push({이름:'주요진',국어:'87',수학:'98',영어:'88',과학:'100'});
-    students.push({이름:'주시진',국어:'87',수학:'98',영어:'88',과학:'100'});
-    students.push({이름:'주지진',국어:'87',수학:'98',영어:'88',과학:'100'});
-    students.push({이름:'주비진',국어:'87',수학:'98',영어:'88',과학:'100'});
-    students.push({이름:'주수진',국어:'87',수학:'98',영어:'88',과학:'100'});
-    students.push({이름:'주재진',국어:'87',수학:'98',영어:'88',과학:'100'});
-    students.push({이름:'주기진',국어:'90',수학:'100',영어:'80',과학:'50'});
-    students.push({이름:'주세진',국어:'87',수학:'98',영어:'88',과학:'100'});
-    students.push({이름:'주추진',국어:'87',수학:'98',영어:'88',과학:'100'});
+    students.push({이름:'주유진',국어:87,수학:98,영어:88,과학:100});
+    students.push({이름:'주요진',국어:87,수학:98,영어:88,과학:100});
+    students.push({이름:'주시진',국어:87,수학:98,영어:88,과학:100});
+    students.push({이름:'주지진',국어:87,수학:98,영어:88,과학:100});
+    students.push({이름:'주비진',국어:87,수학:98,영어:88,과학:100});
+    students.push({이름:'주수진',국어:87,수학:98,영어:88,과학:100});
+    students.push({이름:'주재진',국어:87,수학:98,영어:88,과학:100});
+    students.push({이름:'주기진',국어:90,수학:100,영어:80,과학:50});
+    students.push({이름:'주세진',국어:87,수학:98,영어:88,과학:100});
+    students.push({이름:'주추진',국어:87,수학:98,영어:88,과학:100});
     
     for (var i in students){
       students[i].getSum = function(){
@@ -112,3 +112,103 @@ output += 'total:' + (student.Korean + student.Math + student.English + student.
       }
     }
     alert(output);
+ */
+//함수를 사용한 객체 생성
+/* function makeStudent(name, korean, math, english, science) {
+  var willReturn = {
+    이름 : name,
+    국어 : korean,
+    수학 : math,
+    영어 : english,
+    과학 : science,
+
+    getSum : function () {
+      return this.국어 + this.수학 + this.영어 + this.과학;
+    },
+    getAverage : function () {
+      return this.getSum()/4;
+    },
+    toString : function () {
+      return this.이름 + "\t" + this.getSum() + "\t" + this.getAverage();
+    }
+  };
+  return willReturn;
+}
+
+var student = [];
+student.push(makeStudent("주유진", 100, 100, 100, 100))
+student.push(makeStudent("주지진", 50, 100, 50, 97))
+
+var output = "이름\t총점\t평균\n";
+for (var i in student){
+  output += student[i].toString() + "\n";
+}
+alert(output);
+ */
+
+//생성자 함수
+/* function Student(name, korean, math, english, science) {
+  this.이름 = name,
+  this.국어 = korean,
+  this.수학 = math,
+  this.영어 = english,
+  this.과학 = science,
+  this.getSum = function () {
+    return this.국어 + this.수학 + this.영어 + this.과학;
+  },
+  this.getAverage = function () {
+    return this.getSum()/4;
+  },
+  this.toString = function () {
+    return this.이름 + "\t" + this.getSum() + "\t" + this.getAverage();
+  }
+}
+
+var student = [];
+student.push(new Student("주유진", 100, 100, 100, 100));
+student.push(new Student("주지진", 50, 100, 50, 97));
+
+var output = "이름\t총점\t평균\n";
+for (var i in student){
+  output += student[i].toString() + "\n";
+}
+alert(output); */
+
+//프로토타입
+function Student(name, korean, math, english, science) {
+  this.이름 = name,
+  this.국어 = korean,
+  this.수학 = math,
+  this.영어 = english,
+  this.과학 = science
+  /* this.getSum = function () {
+    return this.국어 + this.수학 + this.영어 + this.과학;
+  },
+  this.getAverage = function () {
+    return this.getSum()/4;
+  },
+  this.toString = function () {
+    return this.이름 + "\t" + this.getSum() + "\t" + this.getAverage();
+  } */
+}
+Student.prototype.getSum = function () {
+  return this.국어 + this.수학 + this.영어 + this.과학;
+}
+Student.prototype.getAverage = function () {
+  return this.getSum()/4;
+}
+Student.prototype.toString = function () {
+  return this.이름 + "\t" + this.getSum() + "\t" + this.getAverage();
+}
+
+
+var student = [];
+student.push(new Student("주유진", 100, 100, 100, 100));
+student.push(new Student("주지진", 50, 100, 50, 97));
+
+var output = "이름\t총점\t평균\n";
+for (var i in student){
+  output += student[i].toString() + "\n";
+}
+alert(output);
+
--- object_01.md
+++ object_01.md
@@ -234,3 +234,119 @@ for (var i in students){
 alert(output);
 ```
 
+
+
+### 6.7 함수를 사용한 객체의 생성
+
+```
+function makeStudent(name, korean, math, english, science) {
+  var willReturn = {
+    이름 : name,
+    국어 : korean,
+    수학 : math,
+    영어 : english,
+    과학 : science,
+
+    getSum : function () {
+      return this.국어 + this.수학 + this.영어 + this.과학;
+    },
+    getAverage : function () {
+      return this.getSum()/4;
+    },
+    toString : function () {
+      return this.이름 + "\t" + this.getSum() + "\t" + this.getAverage();
+    }
+  };
+  return willReturn;
+}
+
+var student = [];
+student.push(makeStudent("주유진", 100, 100, 100, 100))
+student.push(makeStudent("주지진", 50, 100, 50, 97))
+
+var output = "이름\t총점\t평균\n";
+for (var i in student){
+  output += student[i].toString() + "\n";
+}
+alert(output);
+```
+
+
+
+### 6.8 생성자 함수
+
+```\
+function Student(name, korean, math, english, science) {
+  this.이름 = name,
+  this.국어 = korean,
+  this.수학 = math,
+  this.영어 = english,
+  this.과학 = science,
+  this.getSum = function () {
+    return this.국어 + this.수학 + this.영어 + this.과학;
+  },
+  this.getAverage = function () {
+    return this.getSum()/4;
+  },
+  this.toString = function () {
+    return this.이름 + "\t" + this.getSum() + "\t" + this.getAverage();
+  }
+}
+
+var student = [];
+student.push(new Student("주유진", 100, 100, 100, 100));
+student.push(new Student("주지진", 50, 100, 50, 97));
+
+var output = "이름\t총점\t평균\n";
+for (var i in student){
+  output += student[i].toString() + "\n";
+}
+alert(output);
+```
+
+6.8 생성자 함수와 6.7 함수를 사용한 객체의 생성은 겉으로 보았을 때 다를 바 없다.
+
+하지만 6.9 프로토타입을 살펴보면 다른 점을 알 수 있을 것이다. 
+
+
+
+### 6.9 프로토타입
+
+```
+function Student(name, korean, math, english, science) {
+  this.이름 = name,
+  this.국어 = korean,
+  this.수학 = math,
+  this.영어 = english,
+  this.과학 = science
+  /* this.getSum = function () {
+    return this.국어 + this.수학 + this.영어 + this.과학;
+  },
+  this.getAverage = function () {
+    return this.getSum()/4;
+  },
+  this.toString = function () {
+    return this.이름 + "\t" + this.getSum() + "\t" + this.getAverage();
+  } */
+}
+Student.prototype.getSum = function () {
+  return this.국어 + this.수학 + this.영어 + this.과학;
+}
+Student.prototype.getAverage = function () {
+  return this.getSum()/4;
+}
+Student.prototype.toString = function () {
+  return this.이름 + "\t" + this.getSum() + "\t" + this.getAverage();
+}
+```
+
+프로토타입은 생성자 함수만이 가지는 것으로,
+
+프로토타입 사용을 통해 메서드를 한번만 정의하므로 메모리 사용에 있어 효율적이다.
+
+
+
+### 6.10 instanceof
+
+
+
