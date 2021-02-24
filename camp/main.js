9f329cc13c84f0bbe352992766bf5d3fb539c42c
grace <gracejoo26@gmail.com>
2021-02-24 21:17:22 +0900

    ADD campFolder

---
+++ camp/main.js
@@ -0,0 +1,25 @@
+//string(문자 데이터)
+//따옴표를 사용합니다
+
+let myName = "Heropy";
+let email = 'thesecon@gmail.com';
+let hello = `Hello ${myName}`; //보관법
+
+console.log(hello);
+
+//number (숫자 데이터)
+let number = 123;
+let opacity = 1.57;
+
+//boolean
+//true, false
+let checked = true;
+let isShow = false;
+
+//undefined
+//값이 할당되지 않은 상태
+let undef;
+let obj = { abc: 123 }; //객체 data는 중괄호로 여닫기 : 속성에 숫자 데이터를 삽입시켜줌
+
+
+console.log(undef);
